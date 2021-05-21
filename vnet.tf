7764d25d7b79c4cb38c48997440712c205ebeb30
Shwetha92 <shwetharam4@gmail.com>
2021-05-21 10:08:25 +0000

    Added vnet module

--- vnet.tf
+++ vnet.tf
@@ -1,6 +1,15 @@
+
+resource "azurerm_resource_group" "myresourcegroup1" {
+  name     = "${var.prefix}-workshop"
+  location = var.location
+
+  tags = {
+    environment = "Production"
+  }
+}
 resource "azurerm_virtual_network" "Gaurav" {
   name                = "${var.prefix}-vnet"
-  location            = azurerm_resource_group.myresourcegroup.location
+  location            = azurerm_resource_group.myresourcegroup1.location
   address_space       = [var.address_space]
-  resource_group_name = azurerm_resource_group.myresourcegroup.name
+  resource_group_name = azurerm_resource_group.myresourcegroup1.name
 }
