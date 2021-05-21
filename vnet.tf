61694932ec83e3e9c8735f049ebbd4c03a8f25db
Shwetha92 <shwetharam4@gmail.com>
2021-05-21 10:41:41 +0000

    Added vnet module

--- vnet.tf
+++ vnet.tf
@@ -2,5 +2,14 @@ module "network" {
   source  = "Shwetha92/network/azurerm"
   version = "3.0.1"
   # insert the 1 required variable here
-  resource_group_name = "myresourcegroup"
+  resource_group_name = "myresourcegroup1"
+}
+
+resource "azurerm_resource_group" "myresourcegroup1" {
+  name     = "${var.prefix}-workshop"
+  location = var.location
+
+  tags = {
+    environment = "Production"
+  }
 }
