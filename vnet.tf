8ffd4aca7b104b4c96f8fecf2477c52afe0445d8
Shwetha92 <shwetharam4@gmail.com>
2021-05-21 10:02:14 +0000

    Added vnet module

--- vnet.tf
+++ vnet.tf
@@ -1,6 +1,6 @@
-resource "azurerm_virtual_network" "vnet1" {
+resource "azurerm_virtual_network" "Gaurav" {
   name                = "${var.prefix}-vnet"
   location            = azurerm_resource_group.myresourcegroup.location
   address_space       = [var.address_space]
-  resource_group_name = "Gaurav"
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
 }
